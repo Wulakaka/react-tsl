16f4f6f6ff51300a657f141c3d8bcd2ecc74bbc4
zhang.weifeng <zhangweifeng@diinom.com>
2026-03-16 15:19:35 +0800

    feat: 增加 Outline Post-processing 示例

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 // import "./index.css";
-import App from "./PostApp.tsx";
+import App from "./OutlineApp.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
